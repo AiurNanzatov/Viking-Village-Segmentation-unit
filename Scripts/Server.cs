9ed200530238e96c58f800b78a58f3b63c5b56ba
AiurNanzatov <57374474+AiurNanzatov@users.noreply.github.com>
2021-07-28 17:59:27 -0230

    Add files via upload

---
+++ Scripts/Server.cs
@@ -0,0 +1,134 @@
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Server : MonoBehaviour
+{
+
+    private const int MAX_USER = 100;
+    private const int PORT = 26000;
+    private const int WEB_PORT = 26001;
+    private const int BYTE_SIZE = 1024;
+
+    private byte reliableChannel;
+    private int hostId;
+    private int webHostId;
+
+    private bool isStarted;
+    private byte error;
+
+    #region Monobehaviour
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        Init();
+    }
+    private void Update()
+    {
+        UpdateMessagePump();
+    }
+    #endregion
+
+
+    public void Init()
+    {
+        NetworkTransport.Init();
+
+        ConnectionConfig cc = new ConnectionConfig();
+        reliableChannel = cc.AddChannel(QosType.Reliable);
+
+        HostTopology topo = new HostTopology(cc, MAX_USER);
+
+        // Server only
+        hostId = NetworkTransport.AddHost(topo, PORT, null);
+        webHostId = NetworkTransport.AddWebsocketHost(topo, PORT, null);
+
+        Debug.Log(string.Format("Connection on port {0} and webport {1}", PORT, WEB_PORT));
+        isStarted = true;
+    }
+    public void Shutdown()
+    {
+        isStarted = false;
+        NetworkTransport.Shutdown();
+    }
+
+    public void UpdateMessagePump()
+    {
+        if (!isStarted)
+            return;
+
+        int recHostId;      //web or standalone
+        int connectionId;   //Who sends this
+        int channelId;      //Which channel is he sending the message from
+
+        byte[] recBuffer = new byte[BYTE_SIZE];
+        int dataSize;
+
+        NetworkEventType type = NetworkTransport.Receive(out recHostId, out connectionId, out channelId, recBuffer, BYTE_SIZE, out dataSize, out error);
+        switch (type)
+        {
+            case NetworkEventType.Nothing:
+                break;
+
+            case NetworkEventType.ConnectEvent:
+                Debug.Log(string.Format("User {0} has connected through host {1}", connectionId, recHostId) );
+                break;
+
+            case NetworkEventType.DisconnectEvent:
+                Debug.Log(string.Format("User {0} has disconnected!", connectionId));
+                break;
+
+            case NetworkEventType.DataEvent:
+                BinaryFormatter formatter = new BinaryFormatter();
+                MemoryStream ms = new MemoryStream(recBuffer);
+                NetMsg msg = (NetMsg)formatter.Deserialize(ms);
+
+                OnData(connectionId, channelId, recHostId, msg);
+                break;
+
+            default:
+            case NetworkEventType.BroadcastEvent:
+                Debug.Log("Unexpected network event type");
+                break;
+        }
+    }
+    #region OnData
+    private void OnData(int cnnId, int channelId, int recHostId, NetMsg msg)
+    {
+        switch (msg.OP)
+        {
+            case NetOP.None:
+                Debug.Log("Unexpected NetOP");
+                break;
+
+            case NetOP.CreateAccount:
+                CreateAccount(cnnId, channelId, recHostId, (Net_CreateAccount)msg);
+                break;
+        }
+    }
+
+    private void CreateAccount(int cnnId, int channelId, int recHostId, Net_CreateAccount ca)
+    {
+        Debug.Log(string.Format("{0},{1},{2}", ca.Username, ca.Password, ca.Email));
+    }
+    #endregion
+
+    #region Send
+    public void SendClient(int recHost, int cnnId, NetMsg msg)
+    {
+        byte[] buffer = new byte[BYTE_SIZE];
+
+        BinaryFormatter formatter = new BinaryFormatter();
+        MemoryStream ms = new MemoryStream(buffer);
+        formatter.Serialize(ms, msg);
+
+        if (recHost == 0)
+            NetworkTransport.Send(hostId, cnnId, reliableChannel, buffer, BYTE_SIZE, out error);
+        else
+            NetworkTransport.Send(webHostId, cnnId, reliableChannel, buffer, BYTE_SIZE, out error);
+    }
+    #endregion
+}
---
+++ Scripts/Shared/NetMsg.cs
@@ -0,0 +1,17 @@
+public static class NetOP
+{
+    public const int None = 0;
+
+    public const int CreateAccount = 1;
+}
+
+[System.Serializable]
+public abstract class NetMsg
+{
+   public byte OP { set; get; }
+
+    public NetMsg()
+    {
+        OP = NetOP.None;
+    }
+}
